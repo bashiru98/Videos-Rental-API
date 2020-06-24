561cd4ab3bc02074be82e91fd5bdfe50dd6c2d24
Bashiru <atulebashirubukari@gmail.com>
2020-06-24 13:25:26 +0300

    modified

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+const Joi=require('joi');
+Joi.objectId =require('joi-objectid')(Joi)
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
